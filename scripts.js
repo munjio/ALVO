8e6cf783ba5a23a37d84354ac74112d82a0edfac
seanmarkham1988 <64798965+seanmarkham1988@users.noreply.github.com>
2020-05-18 23:29:50 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 // JavaScript Document
 $(document).ready(function(e) {
+	$('#languagechange').change(function(e) {
+        if($(this).val() == "1"){
+			window.location.href = "../en/index.html";
+		}else if($(this).val() == "2"){
+			window.location.href = "../fr/index.html";
+		}else if($(this).val() == "3"){
+			window.location.href = "../es/index.html";
+		}
+    });
 });
+
