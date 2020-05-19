63a812050ac3a99bd889662d097cf996188ea046
seanmarkham1988 <64798965+seanmarkham1988@users.noreply.github.com>
2020-05-19 22:22:10 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,5 +9,53 @@ $(document).ready(function(e) {
 			window.location.href = "../es/index.html";
 		}
     });
-});
+	
+	$('.confirm_btn').click(function(e) {
+        if($('.modal_language').val() == "1"){
+			window.location.href = "../en/index.html";
+		}else if($('.modal_language').val() == "2"){
+			window.location.href = "../fr/index.html";
+		}else if($('.modal_language').val() == "3"){
+			window.location.href = "../es/index.html";
+		}
+    });
 
+	
+	function openModal($modal) {
+  // Note: fixed elements will also need the margin 
+  // adjustment (like a fixed header, if you have one).
+  var scrollBarWidth = window.innerWidth - document.body.offsetWidth;
+  $('body')
+    .css('margin-right', scrollBarWidth)
+    .addClass('showing-modal');
+  $modal.show();
+};
+
+function closeModal($modal) {
+    $('body')
+      .css('margin-right', '')
+      .removeClass('showing-modal');
+    $modal.hide();
+};
+	
+	var $modal = $('#myModal');
+
+// Clicking outside the inner modal content should close it.
+$modal
+	.click(function () {
+		closeModal($modal);
+	})
+  .find('.modal-content').click(function (event) {
+  	event.stopPropagation();
+	});
+	
+	$('.close').click(function () {
+		closeModal($modal);
+	})
+	
+	$('.flag').click(function(e) {
+		 openModal($modal);
+    });
+	
+	
+});
